a75840d85562f631c12312d29068001c956c8a90
Pierre Convert <pierreconvert@macbook-pro-6.home>
2024-05-21 20:30:47 +0200

    Updates

--- engineering.js
+++ engineering.js
@@ -15,14 +15,41 @@ export const WEB_PAGE = page({
             theme: "dark", background: "black",
             title: `<b>Built-in</b> engineering capabilities`,
             subtitle: `The Alphaventor platform came with efficient libraries to power your engineering app.`,
-            paragraph: `Libraries enables multiple features`,
+            paragraph: `Libraries enables complex applications capabilities out of the boxes, greatly 
+            reducing the development time and platform integration`,
             asset: "assets/turbos/SiTyphoon-config-low.png"
         }),
         slide("prime", {
-            theme: "dark", background: "black",
-            title: `Say Hello to <span class="emphasis">SiTy8000u</span> "Silent Typhoon"`,
-            subtitle: `The world's first heavy duty eVTOL propeller`,
-            asset: "assets/logos/Qx-blocks-logo.png"
+            theme: "dark",
+            background: "pic:assets/backgrounds/skies-background-01-low.jpg",
+            title: `Qx-ng-aircrafts`,
+            subtitle: `Blade element analysis: A powerful tool for the low-latitude economy`,
+            paragraph: `Blade element analyis is especially powerful when it comes to calculate quickly 
+            -and accurately- the power/torque of propellers (whether thrusters or pushers) for the analysis
+            of take-off capabilities, entire flight analysis. They offer near instant alternative to CFD and 
+            can therefore be easily integrated into repeatable routines that require speed and stability.`,
+            asset: "assets/aircrafts/evtol-taking-off05-low.png"
+        }),
+        slide("prime", {
+            theme: "light",
+            background: "white",
+            title: `Thermodynamics`,
+            subtitle: `The foundation layer for process engineering`,
+            paragraph: `Being able to compute the thermodynamics properties of process gas (water, steam, natural gas, 
+                refrigerants, thermal oil) is at the heart of many common simulations of equipment. It is more specifically 
+                useful when it comes to predict the behaviour of fluid handling equipement in a specific customer case, be it a 
+                pump, a valve, a turbine or a compressor.`,
+            asset: "assets/applications/power-plant00.png",
+        }),
+        slide("prime", {
+            theme: "light",
+            background: "pic:assets/applications/meshing.png",
+            title: `Thermodynamics`,
+            subtitle: `The foundation layer for process engineering`,
+            paragraph: `Being able to compute the thermodynamics properties of process gas (water, steam, natural gas, 
+                refrigerants, thermal oil) is at the heart of many common simulations of equipment. It is more specifically 
+                useful when it comes to predict the behaviour of fluid handling equipement in a specific customer case, be it a 
+                pump, a valve, a turbine or a compressor.`
         }),
         slide("prime", {
             theme: "dark", background: "black",
@@ -38,11 +65,10 @@ export const WEB_PAGE = page({
             ]
         }),
         slide("prime", {
-            theme: "dark",
-            background: "pic:assets/backgrounds/skies-background-01-low.jpg",
+           
             title: `Opening a new area for eVTOL`,
             subtitle: `Thrust up to 46kN`,
-            asset: "assets/aircrafts/evtol-taking-off05-low.png"
+            
         }),
         slide("paragraph", {
             theme: "dark", background: "pic:assets/capabilities/CFD-Screenshot-02.jpg",
--- platform.js
+++ platform.js
@@ -38,7 +38,6 @@ export const WEB_PAGE = page({
             subtitle: `Gain value beyond your product inherent value.`,
             paragraph: `Delivering a great product is an achievement. Seamlessly integrate in your customers assemblies is 
             paramount, like going from good to unavoidable.`,
-            arrangement: "text-left",
             asset: "assets/slides/slide04.png"
         }),
         slide("prime", {
